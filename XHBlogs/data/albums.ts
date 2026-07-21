746d8d7bd4def4f9d82a26936f64d44ca9371803
jaychou <jaychou8421@gmail.com>
2026-07-21 23:20:42 +0800

    05

--- XHBlogs/data/albums.ts
+++ XHBlogs/data/albums.ts
@@ -40,5 +40,162 @@ export const albums: Album[] = [
         "caption": "飞檐翘角"
       }
     ]
+  },
+  {
+    "id": "nanchang-journey",
+    "title": "南昌行",
+    "description": "一些旅游照片",
+    "cover": "https://bu.dusays.com/2026/07/20/6a5d03512d843.png",
+    "date": "2026.07.21",
+    "photos": [
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f83dfb08a1.jpg",
+        "caption": "艺术"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f83f164196.jpg",
+        "caption": "佛"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f83f7abd94.jpg",
+        "caption": "釉色敦煌"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f865ce2263.jpg",
+        "caption": "滕王阁模型01"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f866118e3a.jpg",
+        "caption": "树雕"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f865ce2263.jpg",
+        "caption": "滕王阁模型01"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f866976e2c.jpg",
+        "caption": "滕王阁模型02"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f866b6fbd6.jpg",
+        "caption": "滕王阁模型03"
+      },
+       {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8735b4a9a.jpg",
+        "caption": "座山雕01"
+      },
+       {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f873aac83c.jpg",
+        "caption": "座山雕02"
+      },
+       {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f873d96cd7.jpg",
+        "caption": "马踏飞燕"
+      },
+       {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f87412c920.jpg",
+        "caption": "骏马奔腾"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f874230b83.jpg",
+        "caption": "关公"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f87440a836.jpg",
+        "caption": "节节高"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f88ae6486c.jpg",
+        "caption": "一群羊"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f88bb5908e.jpg",
+        "caption": "垂钓者"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f88be996d4.jpg",
+        "caption": "蛟骨生春"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f88c6807af.jpg",
+        "caption": "龙凤万花瓶"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f88cc62018.jpg",
+        "caption": "我也不知道"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f88ccc0ca2.jpg",
+        "caption": "五颜六色"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8ad129963.jpg",
+        "caption": "阿格与滤镜系列"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8ad877b11.jpg",
+        "caption": "花开富贵"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8adcd65a9.jpg",
+        "caption": "抽象艺术01"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8add9d3a3.jpg",
+        "caption": "方圆之间"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8adea34f8.jpg",
+        "caption": "抽象艺术02"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8bfe74b45.jpg",
+        "caption": "船"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8c0ba81e9.jpg",
+        "caption": "锦绣中华"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8c11aa7da.jpg",
+        "caption": "红船精神"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8c11c83ab.jpg",
+        "caption": "天道酬勤"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8c1206525.jpg",
+        "caption": "锦绣前程"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8c121c433.jpg",
+        "caption": "江南滕景"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8d3e86d39.jpg",
+        "caption": "滕王阁01"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8d3fd59bb.jpg",
+        "caption": "瓷船"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8d4040b7a.jpg",
+        "caption": "万寿街"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8d4745d99.jpg",
+        "caption": "景屏"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8d4ba5820.jpg",
+        "caption": "滕王阁门"
+      },
+      {
+        "url": "https://bu.dusays.com/2026/07/21/6a5f8d4e5b68e.jpg",
+        "caption": "滕王阁02"
+      }
+    ]
   }
 ];
